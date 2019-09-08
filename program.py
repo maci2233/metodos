5be2f9002dc198ded0cc2d7dedf8f2d4d90212d4
Alfonso Diaz <alfonsodiazinfante@hotmail.com>
2019-09-08 17:30:12 -0500

    tKinter

--- program.py
+++ program.py
@@ -8,6 +8,7 @@ N = 100
 with open('random_nums.txt', 'w') as f:
     for _ in range(N):
         rand_num = ((a * x0 + c) % m) / m
+        print(rand_num)
         f.write('{}\n'.format(rand_num))
         x0 = rand_num
 
